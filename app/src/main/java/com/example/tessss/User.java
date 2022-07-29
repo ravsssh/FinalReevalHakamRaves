d9545df6cb075881f1c3761ef117fd188ceb3252
ravsssGG <raves.hakam@gmail.com>
2022-07-29 21:43:16 +0700

    initiate

---
+++ app/src/main/java/com/example/tessss/User.java
@@ -0,0 +1,17 @@
+package com.example.tessss;
+
+public class User {
+
+    String firstName, lastName, age;
+
+    public String getFirstName() {    return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+}
